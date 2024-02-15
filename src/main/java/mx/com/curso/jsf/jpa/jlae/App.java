b6c53b9b95f85ab04407e57577dedbd1a03efc37
aejluis78 <aejluis@gmail.com>
2024-02-15 11:23:20 -0600

    Version inicial del proyecto Faces an JPA

---
+++ src/main/java/mx/com/curso/jsf/jpa/jlae/App.java
@@ -0,0 +1,13 @@
+package mx.com.curso.jsf.jpa.jlae;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/mx/com/curso/jsf/jpa/jlae/AppTest.java
@@ -0,0 +1,20 @@
+package mx.com.curso.jsf.jpa.jlae;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
